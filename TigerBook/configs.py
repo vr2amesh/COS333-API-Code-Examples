23fed58294bb209ce543c8cedbc2fed54934ab45
Vinay <vramesh@princeton.edu>
2020-04-13 00:51:16 -0400

    Update configs.py

--- TigerBook/configs.py
+++ TigerBook/configs.py
@@ -2,6 +2,8 @@
 # https://tigerbook.herokuapp.com/api/v1/getkey/cos333APIcodeExamples
 # Please use your own keys in your code and application. You never
 # know when this KEY will be overwritten!!!
+# I was logged in as cos333_spr2020 when routing to the above 
+# link and thus the following key was produced
 KEY="fcdc328dcf3deba5dd40fb7e95c07cb8"
 USERNAME="cos333_spr2020"
 AGENT="cos333APIcodeExamples"
