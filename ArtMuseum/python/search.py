c22b5a8920b5f8794960feff7fdd7cb66b0a8f00
Vinay <vramesh@princeton.edu>
2020-05-05 21:53:54 -0400

    added example for ArtMuseum search

--- ArtMuseum/python/search.py
+++ ArtMuseum/python/search.py
@@ -1,2 +1,18 @@
+from configs import SEARCH
+from req_lib import getJSON
+
+'''
+One can use this search endpoint in
+order to search for objects according 
+to their type among other things. 
+The parameters are as follows:
+
+q: query string
+type: object type (this can be
+artobjects, makers, packages, or all)
+
+'''
+
 if __name__ == "__main__":
-    pass
+    req = getJSON(SEARCH, q="blue", type="artobjects")
+    print(req)
