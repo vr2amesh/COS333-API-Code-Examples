7549b14bbf9a60b0b217614c65f3fd11be9165e1
Vinay <vramesh@princeton.edu>
2020-05-05 22:01:10 -0400

    setup configs and directory for java code of ArtMuseum

--- ArtMuseum/java/Configs.java
+++ ArtMuseum/java/Configs.java
@@ -1,5 +1,21 @@
 public class Configs {
+
+    public String base_url;
+    public String objects;
+    public String makers;
+    public String packages;
+    public String search;
+
+    public Configs() {
+        this.base_url = "https://data.artmuseum.princeton.edu";
+        this.objects = "/objects/";
+        this.makers = "/makers/";
+        this.packages = "/packages/";
+        this.search = "/search";
+    }
+
     public static void main(String[] args) {
-        
+        Configs configs = new Configs();
+        System.out.println(configs.base_url);
     }
 }
